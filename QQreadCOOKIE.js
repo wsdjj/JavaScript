cf79d3d0ced51e69eaab51acb01f63b7cc6291ef
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-02-22 04:02:30 +0000

    Update QQreadCOOKIE.js

--- QQreadCOOKIE.js
+++ QQreadCOOKIE.js
@@ -1,5 +1,11 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let QQreadurlVal = ``
+let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywqEPKYXCkYx&ywguid=120195765003&usid=42_mBqiH__6eJd_poi-exX8dKKz3iW7kHBP6_AnV4rkkefvLsP8pVh3DtynwjoUtECnvdXyPFkXnLfJB0v-VC1HGx0dcH_SVBsec-0jAqBpIGM
+
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywMKU8SvHTso&ywguid=3202873963
+
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=yworydKOPvPI&ywguid=3475479463
+
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywKnINKJanjF&ywguid=3461109828`
 
 
 
