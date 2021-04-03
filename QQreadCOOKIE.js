65d6731cdf430fc4837a978b10d0024c29b74dba
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-04-03 02:04:23 +0000

    Update QQreadCOOKIE.js

--- QQreadCOOKIE.js
+++ QQreadCOOKIE.js
@@ -2,7 +2,8 @@
 let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywngUC38EL0F&ywguid=120195765003&usid=43__PYORabrxt041ZKGDoQqIU37Ln4Ug2nLLfglItIAJr7A98GIuh4olv0kwyTxXyFHMFwndQJPp64CeWZgaUSKZM0NCcZuByd58LPuO4mtLvg	
 https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywD7QrDelt5R&ywguid=3461109828
 https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywMkVBUhPCc5&ywguid=3475479463
-https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywwstbpuOKCo&ywguid=3202873963","QQreadurl5":"https://iostgw6.reader.qq.com/v7_5_9/userinfo?usid=ywBCd2Lk9Bm8&uid=854029877765
+https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywwstbpuOKCo&ywguid=3202873963 
+https://iostgw6.reader.qq.com/v7_5_9/userinfo?usid=ywBCd2Lk9Bm8&uid=854029877765
 https://newios.reader.qq.com/v7_5_9/userinfo?usid=ywbfGB34Gw6R&uid=800218817607`
 
 
