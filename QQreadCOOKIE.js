7e97ef258d80b5881389ac0437654adbc00f49c0
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-04-03 01:41:39 +0000

    Update QQreadCOOKIE.js

--- QQreadCOOKIE.js
+++ QQreadCOOKIE.js
@@ -1,8 +1,9 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywqEPKYXCkYx&ywguid=120195765003&usid=42_mBqiH__6eJd_poi-exX8dKKz3iW7kHBP6_AnV4rkkefvLsP8pVh3DtynwjoUtECnvdXyPFkXnLfJB0v-VC1HGx0dcH_SVBsec-0jAqBpIGM	
-https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywMKU8SvHTso&ywguid=3202873963
-https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=yworydKOPvPI&ywguid=3475479463
-https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywKnINKJanjF&ywguid=3461109828`
+let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywngUC38EL0F&ywguid=120195765003&usid=43__PYORabrxt041ZKGDoQqIU37Ln4Ug2nLLfglItIAJr7A98GIuh4olv0kwyTxXyFHMFwndQJPp64CeWZgaUSKZM0NCcZuByd58LPuO4mtLvg	
+https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywD7QrDelt5R&ywguid=3461109828
+https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywMkVBUhPCc5&ywguid=3475479463
+https://iostgw6.reader.qq.com/v7_5_9/userinfo?ywkey=ywwstbpuOKCo&ywguid=3202873963","QQreadurl5":"https://iostgw6.reader.qq.com/v7_5_9/userinfo?usid=ywBCd2Lk9Bm8&uid=854029877765
+https://newios.reader.qq.com/v7_5_9/userinfo?usid=ywbfGB34Gw6R&uid=800218817607`
 
 
 
