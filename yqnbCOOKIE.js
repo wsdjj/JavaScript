f7976522866b5e67eae3669d5c54ad8605ea395a
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-04-15 12:29:10 +0800

    Create yqnbCOOKIE.js

---
+++ yqnbCOOKIE.js
@@ -0,0 +1,21 @@
+/* ziye
+独立COOKIE文件
+github地址 https://github.com/ziye888
+TG频道地址  https://t.me/ziyescript
+TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
+boxjs链接  https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye.boxjs.json
+圈X task订阅 https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye-gallery.json
+转载请备注个名字，谢谢
+在boxjs里选择复制会话 粘贴至cookie处
+*/
+
+
+
+
+let cookie = {"yqnbheader":"BJYADMIN=hf0ih1kr09ff88rnrj892od3l4","yqnbbody":"9299","yqnbheader2":"","yqnbbody2":"","yqnbheader3":"","yqnbbody3":"","yqnbheader4":"","yqnbbody4":"","yqnbheader5":"","yqnbbody5":"","yqnbheader6":"","yqnbbody6":"","yqnbheader7":"","yqnbbody7":"","yqnbheader8":"","yqnbbody8":"","yqnbheader9":"","yqnbbody9":"","yqnbheader10":"","yqnbbody10":"","yqnbheader11":"","yqnbbody11":"","yqnbheader12":"","yqnbbody12":"","yqnbheader13":"","yqnbbody13":"","yqnbheader14":"","yqnbbody14":"","yqnbheader15":"","yqnbbody15":"","yqnbheader16":"","yqnbbody16":"","yqnbheader17":"","yqnbbody17":"","yqnbheader18":"","yqnbbody18":"","yqnbheader19":"","yqnbbody19":"","yqnbheader20":"","yqnbbody20":""}
+
+
+
+
+
+module.exports = cookie
