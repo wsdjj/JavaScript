8d7b0825caf196e4f26a18ed5e271c67fd4aad0c
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-05-24 08:19:04 +0800

    Update shzxCOOKIE2.js

--- shzxCOOKIE2.js
+++ shzxCOOKIE2.js
@@ -1,5 +1,14 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let shzxbodyVal = `{   "userId" : "60aa3032df63d700015c5192",   "os" : "2",   "appVersion" : "5.2.3",   "appName" : "2",   "did" : "847c684d878fd73fc70689ed810759e9",   "token" : "eyJleHAiOjE2Mjk1NDIxOTQ2MDksImlhdCI6MTYyMTc2NjE5NDYwOSwicHAiOiIxMzk2NDE0NzQ3MjE3NzAyOTEyQHNvaHUuY29tIiwidGsiOiJ3VXdZc05SdWVVRmtENjU4Z1BLUmlmckxTT1lNeDAyZiIsInYiOjB9.gp99vJIqIrhoAXbvuu9u9IWUl4MxX07ghENQRMTv0Ag",   "timestamp" : 1621766230140,   "sourceType" : 1 }`
+let shzxbodyVal = `{
+  "userId" : "60aa3032df63d700015c5192",
+  "os" : "2",
+  "appVersion" : "5.2.3",
+  "appName" : "2",
+  "did" : "847c684d878fd73fc70689ed810759e9",
+  "token" : "eyJleHAiOjE2Mjk1NDIxOTQ2MDksImlhdCI6MTYyMTc2NjE5NDYwOSwicHAiOiIxMzk2NDE0NzQ3MjE3NzAyOTEyQHNvaHUuY29tIiwidGsiOiJ3VXdZc05SdWVVRmtENjU4Z1BLUmlmckxTT1lNeDAyZiIsInYiOjB9.gp99vJIqIrhoAXbvuu9u9IWUl4MxX07ghENQRMTv0Ag",
+  "timestamp" : 1621766230140,
+  "sourceType" : 1
+}`
 
 
 
